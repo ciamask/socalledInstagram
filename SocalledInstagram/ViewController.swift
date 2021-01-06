5bc706e961d70064167fe3ba799a68381c74dc4b
Shreeya Maskey <maskey.shriya@gmail.com>
2021-01-06 09:00:57 +0545

    Initial Commit

---
+++ SocalledInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocalledInstagram
+//
+//  Created by cia on 06/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
