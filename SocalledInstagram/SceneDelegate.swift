6933ec3c57101990a5d02e9eb62afdd149d7f3ef
Shreeya Maskey <maskey.shriya@gmail.com>
2021-04-22 12:07:15 +0545

    Initial commit

--- SocalledInstagram/SceneDelegate.swift
+++ SocalledInstagram/SceneDelegate.swift
@@ -17,8 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-//        window?.rootViewController = LoginVC()
-        window?.rootViewController = MainTabBarVC()
+        window?.rootViewController = LoginVC()
+//        window?.rootViewController = MainTabBarVC()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
